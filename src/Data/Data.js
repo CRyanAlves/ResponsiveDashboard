3f44292863b64a4bdd0f08a322f366c0dea846d1
RyanAlves26 <ryan.alves@gestaokaizen.com.br>
2024-03-04 09:41:44 -0300

    first commit

---
+++ src/Data/Data.js
@@ -0,0 +1,84 @@
+import {
+  UilEstate,
+  UilClipboardAlt,
+  UilUsersAlt,
+  UilPackage,
+  UilChart,
+} from "@iconscout/react-unicons";
+
+// Analytics Cards imports
+import { UilUsdSquare, UilMoneyWithdrawal } from "@iconscout/react-unicons";
+
+export const SidebarData = [
+  {
+    icon: UilEstate,
+    heading: "Dashboard",
+  },
+  {
+    icon: UilClipboardAlt,
+    heading: "Orders",
+  },
+  {
+    icon: UilUsersAlt,
+    heading: "Customers",
+  },
+  {
+    icon: UilPackage,
+    heading: "Products",
+  },
+  {
+    icon: UilChart,
+    heading: "Analytics",
+  },
+];
+
+export const cardsData = [
+  {
+    title: "Sales",
+    color: {
+      backGround: "linear-gradient(180deg, #141e30 0%, #243b55 100%)",
+      boxShadow: "0px 5px 20px 0px #243b55",
+    },
+    barValue: 70,
+    value: "25,970",
+    png: UilUsdSquare,
+    series: [
+      {
+        name: "Sales",
+        data: [31, 40, 28, 51, 42, 109, 100],
+      },
+    ],
+  },
+  {
+    title: "Revenue",
+    color: {
+      backGround: "linear-gradient(180deg, #141e30 0%, #243b55 100%)",
+      boxShadow: "0px 5px 20px 0px #243b55",
+    },
+    barValue: 80,
+    value: "14,270",
+    png: UilMoneyWithdrawal,
+    series: [
+      {
+        name: "Revenue",
+        data: [10, 100, 50, 70, 80, 30, 40],
+      },
+    ],
+  },
+  {
+    title: "Expenses",
+    color: {
+      backGround: "linear-gradient(180deg, #141e30 0%, #243b55 100%)",
+      boxShadow: "0px 5px 20px 0px #243b55",
+    },
+    barValue: 60,
+    value: "4,270",
+    png: UilClipboardAlt,
+    series: [
+      {
+        name: "Expenses",
+        data: [10, 25, 15, 30, 12, 15, 20],
+      },
+    ],
+  },
+];
